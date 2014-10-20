deab14dcc45183a46327f2fbdab275ffc977d769
Kyla Medina <kylamedina@gmail.com>
2014-10-19 23:10:34 -0400

    updated mute path

--- lib/mute/index.sh
+++ lib/mute/index.sh
@@ -1,9 +1,9 @@
-sudo chmod u+x ./mute-on.sh
-sudo chmod u+x ./mute-off.sh
+sudo chmod u+x /usr/local/lib/dots/lib/mute/mute-on.sh
+sudo chmod u+x /usr/local/lib/dots/lib/mute/mute-off.sh
 
-sudo defaults write com.apple.loginwindow LogoutHook ./mute-on.sh
-sudo defaults write com.apple.loginwindow LoginHook ./mute-off.sh
+sudo defaults write com.apple.loginwindow LogoutHook /usr/local/lib/dots/lib/mute/mute-on.sh
+sudo defaults write com.apple.loginwindow LoginHook /usr/local/lib/dots/lib/mute/mute-off.sh
 
 # To Delete:
 # sudo defaults delete com.apple.loginwindow LoginHook
-# sudo defaults delete com.apple.loginwindow LogoutHook
+# sudo defaults delete com.apple.loginwindow LogoutHook	

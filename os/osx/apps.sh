1baaffcb0c4bb3ef10561250c0048164cc00ce9d
Kyla Medina <hi@ky.la>
2015-12-03 21:10:40 -0500

    moved subl

--- os/osx/apps.sh
+++ os/osx/apps.sh
@@ -76,6 +76,7 @@ cleanup() {
 	cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
 	rm -r User
 	ln -s ~/Dropbox/Sublime/User
+	ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/Sites/dot-settings/bin/subl
 	xcode-select --install
 }
 
